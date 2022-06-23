a6a61286a691031cd82e622045b9f7d426423fd7
FRANCO1357 <fmelani13@gmail.com>
2022-06-23 17:17:48 +0200

    end base

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 // 1. calcolare numeri da 1 a 100
-// 2. individuare i numeri divisibili per 3 e per 5
-// 3. individuare i numeri divisibili sia per 3 che per 5
-// 4. dare a questi numeri classi diverse
-// 5. dare alle classi colori diversi
-// 6. stampare i numeri in console
+// 2. individuare i numeri divisibili per 3
+// 3. individuare i numeri divisibili per 5
+// 4. individuare i numeri divisibili sia per 3 che per 5
+// 5. individuare i numeri non divisibili per 3 e per 5
+// 6. Stampare in console Fizz al posto dei numeri divisibili per 3
+// 7. Stampare in console Buzz al posto dei numeri divisibili per 5
+// 8. Stampare in console FizzBuzz al posto dei numeri divisibili sia per 3 che per 5
+// 9. Stampare in console il numero se non è divisibile ne per 3 ne per 5
 
-const row = document.getElementById('row');
-let listOfNumber = '<ul>';
 
+let number = '';
 
 for (let i = 1; i <= 100; i++){
-    console.log(i);
-    row.innerHTML += `<li>${i}</li>`;
+    number.innerText += + i;
 
     if (i % 3 == 0 && i % 5 == 0){
-        row.innerText += 'red';
+        console.log('FizzBuzz');
     } else if (i % 3 == 0){
-        row.innerText += 'green';
+        console.log('Fizz');
     } else if (i % 5 == 0){
-        row.innerText += 'blue';
+        console.log('Buzz');
+    } else {
+        console.log(i);
     }
 }
-
-listOfNumber = '</ul>';
-
-row.innerHTML = listOfNumber;
